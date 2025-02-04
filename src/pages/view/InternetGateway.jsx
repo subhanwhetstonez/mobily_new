8bed40eea3b0ecbda45e772b094347dc85c3957f
Subhan <subhan.suleman@whetstonez.com>
2025-02-04 20:11:03 +0500

    InternetGateway

---
+++ src/pages/view/InternetGateway.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const InternetGateways = () => {
+  return (
+    <div className="grid gap-10 pt-[6%]">
+      <div className="px-[4%]">
+        <p className="text-[#005FA6] font-[700] text-[2vw]">
+          Internet Gateways
+        </p>
+      </div>
+      <div className="flex justify-center pb-[6%]">
+        <img src="/pages/gatewaymap.svg" />
+      </div>
+    </div>
+  );
+};
+
+export default InternetGateways;
