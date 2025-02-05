a111d133f6c3583cf2a645194fb207e104404909
Subhan <subhan.suleman@whetstonez.com>
2025-02-05 12:59:27 +0500

    Redone changes according to horizontal screen

--- src/pages/view/borderInterconnectPoint.jsx
+++ src/pages/view/borderInterconnectPoint.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const BorderPage = () => {
   return (
     <div>
-      <div className=" w-[100%] font-bold flex flex-col p-[4%] relative">
+      <div className=" w-[100%] font-bold flex flex-col p-[3%] relative">
         <div className="text-[#005FA6] text-[35px] mb-2">
           Terrestrial & Cross Borders
         </div>
@@ -13,12 +13,12 @@ const BorderPage = () => {
         >
           Border Interconnect Point
         </div>
-        <div className="text-[#005FA6] text-[35px] mb-8">
+        <div className="text-[#005FA6] text-[34px] mb-8">
           KSA Terrestrial Crossing
         </div>
       </div>
       <div className="flex justify-center mt-[-120px] z-0">
-        <video autoPlay loop muted playsInline width={"800px"}>
+        <video autoPlay loop muted playsInline width={"700px"}>
           <source src="/pages/001.mp4" />
         </video>
       </div>
