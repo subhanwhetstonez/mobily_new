68f6020e89555704e22ef6602f68f6c099c1b910
Subhan <subhan.suleman@whetstonez.com>
2025-02-05 12:08:02 +0500

    New Changes

--- src/pages/test/index.js
+++ src/pages/test/index.js
@@ -1,22 +1,34 @@
 import React from "react";
+import AnimatedDiv from "../components/AnimatedDiv";
 
-const InternetGateways = () => {
+const TGNAsia = () => {
   return (
     <div>
-      <div className="relative grid gap-10 pt-[6%] bg-[#edf7ff] z-10">
-        <div className="px-[4%]">
-          <p className="text-[#005FA6] font-[700] text-[2vw]">
-            Internet Gateways
+      <div className="relative z-10 flex flex-col gap-10 px-[4vw] pt-[4vw]">
+        <div>
+          <p className="text-[#005FA6] text-[30px] font-[700] ">
+            TGN-Euro Asia
           </p>
         </div>
+        <div className="grid grid-cols-2 gap-4 self-baseline">
+          <div>
+            <AnimatedDiv title={"15,000 Km"} subtitle={"Submarine Cable"} />
+          </div>
+          <div>
+            <AnimatedDiv
+              title={"100 Gbps"}
+              subtitle={"Wave Length Techmology"}
+            />
+          </div>
+        </div>
       </div>
-      <div className="flex justify-center z-0">
+      <div className="flex justify-center mt-[20px] z-0">
         <video autoPlay loop muted playsInline width={"800px"}>
-          <source src="/pages/014.mp4" />
+          <source src="/pages/008.mp4" />
         </video>
       </div>
     </div>
   );
 };
 
-export default InternetGateways;
+export default TGNAsia;
