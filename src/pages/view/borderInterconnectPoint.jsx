2d1da08f279c47e1ee7b583ea84e3d35a3f0b991
Subhan <subhan.suleman@whetstonez.com>
2025-02-05 11:22:06 +0500

    More chnages

--- src/pages/view/borderInterconnectPoint.jsx
+++ src/pages/view/borderInterconnectPoint.jsx
@@ -2,23 +2,26 @@ import React from "react";
 
 const BorderPage = () => {
   return (
-    <div className="bg-[#EDF7FF] w-[100%] font-bold flex flex-col p-[5%] ">
-      <div className="text-[#005FA6] text-[36px] mb-4">
-        Terrestrial & Cross Borders
+    <div>
+      <div className=" w-[100%] font-bold flex flex-col p-[4%] relative">
+        <div className="text-[#005FA6] text-[35px] mb-2">
+          Terrestrial & Cross Borders
+        </div>
+        <div
+          className="text-[#005FA6] w-[300px] h-[60px] bg-[#FFFFFF] text-[20px] font-semibold 
+        flex items-center justify-center mb-4"
+        >
+          Border Interconnect Point
+        </div>
+        <div className="text-[#005FA6] text-[35px] mb-8">
+          KSA Terrestrial Crossing
+        </div>
       </div>
-      <div
-        className="text-[#005FA6] w-[350px] h-[90px] bg-[#FFFFFF] text-[24px] font-semibold 
-                    flex items-center justify-center mb-10"
-      >
-        Border Interconnect Point
+      <div className="flex justify-center mt-[-120px] z-0">
+        <video autoPlay loop muted playsInline width={"800px"}>
+          <source src="/pages/010.mp4" />
+        </video>
       </div>
-      <div className="text-[#005FA6] text-[65px] mb-14">
-        KSA Terrestrial Crossing
-      </div>
-      <img
-        src="/pages/fiberNetwork1.svg"
-        className="h-[779px] w-[581] flex items-center justify-center"
-      />
     </div>
   );
 };
